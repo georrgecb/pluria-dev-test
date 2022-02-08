b4cc259f77b192c19f9f605829d3573e025633a9
georrgecb <bucurescu.george@gmail.com>
2022-02-08 14:31:56 +0200

    Main function updated

--- test_solution/flatObjToMap.ts
+++ test_solution/flatObjToMap.ts
@@ -1,22 +1,21 @@
 export const flatObjToMap = (obj: object) => {
-  // The only reason why I chose typescript was to add map generics
   const newMap = new Map<String, String>();
 
-  const mainFunction = (obj: object, prevKeyName?: string) => {
+  const mainFunction = (obj: object) => {
     Object.keys(obj).forEach((key) => {
-      // I had to google the regex for replacing whitespaces
-      const newKey = prevKeyName
-        ? `${prevKeyName}__${key.replace(/\s+/g, "--")}`
-        : `${key}`;
-
-      // If the value is another object, we repeat this whole process, otherwise we update the new Map.
-      if (typeof obj[key] == "object") {
-        mainFunction(obj[key], newKey);
+      const objTemp = {};
+      if (typeof obj[key] === "object") {
+        Object.keys(obj[key]).forEach((k: string | number) => {
+          const newKey = `${key.replaceAll(" ", "--")}__${k}`;
+          objTemp[newKey] = obj[key][k];
+          return mainFunction(objTemp); // aici am pus initial mainFunction(objTemp[newKey]), motiv pentru care aveam o eroare si practic am stat mai bine de 30 min s-o gasesc
+        });
       } else {
-        newMap.set(newKey, obj[key]);
+        newMap.set(key, obj[key]);
       }
     });
   };
+
   //   Call the function
   mainFunction(obj);
   //   Show results on console log
