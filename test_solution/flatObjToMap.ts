ef6ce1d1b79681583d0be4b2375d00efdeda714d
georrgecb <bucurescu.george@gmail.com>
2022-02-08 14:46:48 +0200

    Main function updated

--- test_solution/flatObjToMap.ts
+++ test_solution/flatObjToMap.ts
@@ -5,11 +5,11 @@ export const flatObjToMap = (obj: object) => {
     Object.keys(obj).forEach((key) => {
       const objTemp = {};
       if (typeof obj[key] === "object") {
-        Object.keys(obj[key]).forEach((k: string | number) => {
+        Object.keys(obj[key]).forEach((k: string) => {
           const newKey = `${key.replaceAll(" ", "--")}__${k}`;
           objTemp[newKey] = obj[key][k];
-          return mainFunction(objTemp); // aici am pus initial mainFunction(objTemp[newKey]), motiv pentru care aveam o eroare si practic am stat mai bine de 30 min s-o gasesc
         });
+        return mainFunction(objTemp); // aici am pus initial mainFunction(objTemp[newKey]), motiv pentru care aveam o eroare si practic am stat mai bine de 30 min s-o gasesc
       } else {
         newMap.set(key, obj[key]);
       }
